be2b704219e6980f9bfdafddfc060e9998330e24
Madziu <radek.kobylecki2006@gmail.com>
2025-10-15 21:47:53 +0200

    Huge fixes, last project wasn't working. Changed from emojis as text to images

---
+++ Memory.Controls/BoardControl.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Memory.Controls
+{
+    partial class BoardControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel = new TableLayoutPanel();
+            tileControl16 = new TileControl();
+            tileControl15 = new TileControl();
+            tileControl14 = new TileControl();
+            tileControl13 = new TileControl();
+            tileControl12 = new TileControl();
+            tileControl11 = new TileControl();
+            tileControl10 = new TileControl();
+            tileControl9 = new TileControl();
+            tileControl8 = new TileControl();
+            tileControl7 = new TileControl();
+            tileControl6 = new TileControl();
+            tileControl5 = new TileControl();
+            tileControl4 = new TileControl();
+            tileControl3 = new TileControl();
+            tileControl2 = new TileControl();
+            tileControl1 = new TileControl();
+            tableLayoutPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 4;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.Controls.Add(tileControl15, 2, 3);
+            tableLayoutPanel.Controls.Add(tileControl14, 1, 3);
+            tableLayoutPanel.Controls.Add(tileControl13, 0, 3);
+            tableLayoutPanel.Controls.Add(tileControl12, 3, 2);
+            tableLayoutPanel.Controls.Add(tileControl11, 2, 2);
+            tableLayoutPanel.Controls.Add(tileControl10, 1, 2);
+            tableLayoutPanel.Controls.Add(tileControl9, 0, 2);
+            tableLayoutPanel.Controls.Add(tileControl8, 3, 1);
+            tableLayoutPanel.Controls.Add(tileControl7, 2, 1);
+            tableLayoutPanel.Controls.Add(tileControl6, 1, 1);
+            tableLayoutPanel.Controls.Add(tileControl5, 0, 1);
+            tableLayoutPanel.Controls.Add(tileControl4, 3, 0);
+            tableLayoutPanel.Controls.Add(tileControl3, 2, 0);
+            tableLayoutPanel.Controls.Add(tileControl2, 1, 0);
+            tableLayoutPanel.Controls.Add(tileControl1, 0, 0);
+            tableLayoutPanel.Controls.Add(tileControl16, 3, 3);
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.Location = new Point(0, 0);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 4;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.Size = new Size(800, 800);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // tileControl16
+            // 
+            tileControl16.Anchor = AnchorStyles.None;
+            tileControl16.Location = new Point(607, 607);
+            tileControl16.Name = "tileControl16";
+            tileControl16.Size = new Size(185, 185);
+            tileControl16.TabIndex = 15;
+            // 
+            // tileControl15
+            // 
+            tileControl15.Anchor = AnchorStyles.None;
+            tileControl15.Location = new Point(407, 607);
+            tileControl15.Name = "tileControl15";
+            tileControl15.Size = new Size(185, 185);
+            tileControl15.TabIndex = 14;
+            // 
+            // tileControl14
+            // 
+            tileControl14.Anchor = AnchorStyles.None;
+            tileControl14.Location = new Point(207, 607);
+            tileControl14.Name = "tileControl14";
+            tileControl14.Size = new Size(185, 185);
+            tileControl14.TabIndex = 13;
+            // 
+            // tileControl13
+            // 
+            tileControl13.Anchor = AnchorStyles.None;
+            tileControl13.Location = new Point(7, 607);
+            tileControl13.Name = "tileControl13";
+            tileControl13.Size = new Size(185, 185);
+            tileControl13.TabIndex = 12;
+            // 
+            // tileControl12
+            // 
+            tileControl12.Anchor = AnchorStyles.None;
+            tileControl12.Location = new Point(607, 407);
+            tileControl12.Name = "tileControl12";
+            tileControl12.Size = new Size(185, 185);
+            tileControl12.TabIndex = 11;
+            // 
+            // tileControl11
+            // 
+            tileControl11.Anchor = AnchorStyles.None;
+            tileControl11.Location = new Point(407, 407);
+            tileControl11.Name = "tileControl11";
+            tileControl11.Size = new Size(185, 185);
+            tileControl11.TabIndex = 10;
+            // 
+            // tileControl10
+            // 
+            tileControl10.Anchor = AnchorStyles.None;
+            tileControl10.Location = new Point(207, 407);
+            tileControl10.Name = "tileControl10";
+            tileControl10.Size = new Size(185, 185);
+            tileControl10.TabIndex = 9;
+            // 
+            // tileControl9
+            // 
+            tileControl9.Anchor = AnchorStyles.None;
+            tileControl9.Location = new Point(7, 407);
+            tileControl9.Name = "tileControl9";
+            tileControl9.Size = new Size(185, 185);
+            tileControl9.TabIndex = 8;
+            // 
+            // tileControl8
+            // 
+            tileControl8.Anchor = AnchorStyles.None;
+            tileControl8.Location = new Point(607, 207);
+            tileControl8.Name = "tileControl8";
+            tileControl8.Size = new Size(185, 185);
+            tileControl8.TabIndex = 7;
+            // 
+            // tileControl7
+            // 
+            tileControl7.Anchor = AnchorStyles.None;
+            tileControl7.Location = new Point(407, 207);
+            tileControl7.Name = "tileControl7";
+            tileControl7.Size = new Size(185, 185);
+            tileControl7.TabIndex = 6;
+            // 
+            // tileControl6
+            // 
+            tileControl6.Anchor = AnchorStyles.None;
+            tileControl6.Location = new Point(207, 207);
+            tileControl6.Name = "tileControl6";
+            tileControl6.Size = new Size(185, 185);
+            tileControl6.TabIndex = 5;
+            // 
+            // tileControl5
+            // 
+            tileControl5.Anchor = AnchorStyles.None;
+            tileControl5.Location = new Point(7, 207);
+            tileControl5.Name = "tileControl5";
+            tileControl5.Size = new Size(185, 185);
+            tileControl5.TabIndex = 4;
+            // 
+            // tileControl4
+            // 
+            tileControl4.Anchor = AnchorStyles.None;
+            tileControl4.Location = new Point(607, 7);
+            tileControl4.Name = "tileControl4";
+            tileControl4.Size = new Size(185, 185);
+            tileControl4.TabIndex = 3;
+            // 
+            // tileControl3
+            // 
+            tileControl3.Anchor = AnchorStyles.None;
+            tileControl3.Location = new Point(407, 7);
+            tileControl3.Name = "tileControl3";
+            tileControl3.Size = new Size(185, 185);
+            tileControl3.TabIndex = 2;
+            // 
+            // tileControl2
+            // 
+            tileControl2.Anchor = AnchorStyles.None;
+            tileControl2.Location = new Point(207, 7);
+            tileControl2.Name = "tileControl2";
+            tileControl2.Size = new Size(185, 185);
+            tileControl2.TabIndex = 1;
+            // 
+            // tileControl1
+            // 
+            tileControl1.Anchor = AnchorStyles.None;
+            tileControl1.Location = new Point(7, 7);
+            tileControl1.Name = "tileControl1";
+            tileControl1.Size = new Size(185, 185);
+            tileControl1.TabIndex = 0;
+            // 
+            // BoardControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel);
+            Name = "BoardControl";
+            Size = new Size(800, 800);
+            tableLayoutPanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel;
+        private TileControl tileControl16;
+        private TileControl tileControl15;
+        private TileControl tileControl14;
+        private TileControl tileControl13;
+        private TileControl tileControl12;
+        private TileControl tileControl11;
+        private TileControl tileControl10;
+        private TileControl tileControl9;
+        private TileControl tileControl8;
+        private TileControl tileControl7;
+        private TileControl tileControl6;
+        private TileControl tileControl5;
+        private TileControl tileControl4;
+        private TileControl tileControl3;
+        private TileControl tileControl2;
+        private TileControl tileControl1;
+    }
+}
---
+++ Memory.Controls/TileControl.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Memory.Controls
+{
+    partial class TileControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox = new PictureBox();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(pictureBox, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Font = new Font("Segoe UI Emoji", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(150, 150);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.None;
+            pictureBox.Location = new Point(25, 25);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(100, 100);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // TileControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "TileControl";
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox;
+    }
+}
